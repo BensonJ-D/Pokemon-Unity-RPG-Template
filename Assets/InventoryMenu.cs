52f97180358b60b7765c001abaed9eeb18cbc929
James Benson <pheonixkatana@gmail.com>
2021-07-06 07:22:13 +0000

    Start inventory dev

---
+++ Assets/InventoryMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryMenu : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> itemSlots;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
